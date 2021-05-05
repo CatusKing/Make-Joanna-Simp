5a12f8135a0118e8e477166118babc2bf95eb739
CactusKing101 <chalkking17@gmail.com>
2021-05-05 11:11:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,20 +23,35 @@ client.on('message', msg => {
   const command = args.shift().toLowerCase();
 
   if (command == 'send') {
-    const i = num(args[0]);
-    var embed = new Discord.MessageEmbed()
-      .setDescription(`Just sent reason #${i + 1} to joanna on why I love them!!!!`)
-      .setColor('#014f41');
-    msg.channel.send(embed);
-    embed = new Discord.MessageEmbed()
-      .setTitle(`Reason #${i + 1} on why I love you!!!!!!!!!!!!!`)
-      .setColor('#014f41')
-      .setAuthor('Thomas', 'https://cdn.discordapp.com/avatars/473110112844644372/26ab29e81d29d6a4c34053688e938559.png?size=256')
-      .setDescription(`I love you because, ${reasons[100][i]}`);
-    const target = client.users.cache.get('576154421579481090');
-    target.send(embed);
-    const cactus = client.users.cache.get('473110112844644372');
-    cactus.send(embed);
+    if (args[0] == 'all' && msg.author.id == '473110112844644372') {
+      let num = 0;
+      for(let i of reasons[100]) {
+        var embed = new Discord.MessageEmbed()
+          .setTitle(`Reason #${++num} on why I love you!!!!!!!!!!!!!`)
+          .setColor('#014f41')
+          .setAuthor('Thomas', 'https://cdn.discordapp.com/avatars/473110112844644372/26ab29e81d29d6a4c34053688e938559.png?size=256')
+          .setDescription(`I love you because, ${i}`);
+        const target = client.users.cache.get('576154421579481090');
+        target.send(embed);
+        const cactus = client.users.cache.get('473110112844644372');
+        cactus.send(embed);
+      }
+    } else {
+      const i = num(args[0]);
+      var embed = new Discord.MessageEmbed()
+        .setDescription(`Just sent reason #${i + 1} to joanna on why I love them!!!!`)
+        .setColor('#014f41');
+      msg.channel.send(embed);
+      embed = new Discord.MessageEmbed()
+        .setTitle(`Reason #${i + 1} on why I love you!!!!!!!!!!!!!`)
+        .setColor('#014f41')
+        .setAuthor('Thomas', 'https://cdn.discordapp.com/avatars/473110112844644372/26ab29e81d29d6a4c34053688e938559.png?size=256')
+        .setDescription(`I love you because, ${reasons[100][i]}`);
+      const target = client.users.cache.get('576154421579481090');
+      target.send(embed);
+      const cactus = client.users.cache.get('473110112844644372');
+      cactus.send(embed);
+    }
   }
 });
 
